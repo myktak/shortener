f9bc4c4fa83a44bbd3405eed4faaafdf92df50c0
MykytaKrupskyi <krupskyj234@gmail.com>
2022-08-19 10:22:54 +0200

    added functionality

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from tkinter import *
 import pyshorteners
 
+def shorten():
+    if shorty.get():
+        shorty.delete(0, END)
+    if entry.get():
+        url = pyshorteners.Shortener().tinyurl.short(entry.get())
+        shorty.insert(END, url)
+
 root = Tk()
 root.title('Shortener(tkinter)')
 root.attributes('-type', 'dialog')
@@ -11,7 +18,7 @@ label.pack(pady=(10, 0))
 entry = Entry(root, font=80)
 entry.pack(padx=20)
 
-button = Button(root, text="Shorten link", font=50)
+button = Button(root, text="Shorten link", font=50, command=shorten)
 button.pack(pady=20, padx=30)
 
 shorty_label = Label(root, text="Shortened link:", font=30)
